8d32a5c7a862271f9aeb71a42b51c7343e908195
mathlete01 <msallin@gmail.com>
2021-01-14 17:07:57 -0800

    Remove Prompt from the db

--- db/schema.rb
+++ db/schema.rb
@@ -20,16 +20,6 @@ ActiveRecord::Schema.define(version: 2021_01_14_231814) do
     t.index ["user_id"], name: "index_credit_card_debts_on_user_id"
   end
 
-  create_table "prompts", force: :cascade do |t|
-    t.integer "comesFrom"
-    t.integer "continueTo"
-    t.boolean "answer"
-    t.integer "yesTo"
-    t.integer "noTo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
