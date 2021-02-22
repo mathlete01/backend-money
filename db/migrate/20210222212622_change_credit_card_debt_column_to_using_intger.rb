e7ec0d141ad2ce6a401fe64ab707d2299c44312c
mathlete01 <msallin@gmail.com>
2021-02-22 13:36:16 -0800

    Tweak migration for credit_card_debt column

--- db/migrate/20210222212622_change_credit_card_debt_column_to_using_intger.rb
+++ db/migrate/20210222212622_change_credit_card_debt_column_to_using_intger.rb
@@ -1,5 +1,6 @@
 class ChangeCreditCardDebtColumnToUsingIntger < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :credit_card_debt, 'integer USING credit_card_debt::integer'
+    # change_column :users, :credit_card_debt, 'integer USING credit_card_debt::integer'
+    ALTER TABLE users ALTER COLUMN credit_card_debt TYPE integer USING (trim(credit_card_debt)::integer);
   end
 end
