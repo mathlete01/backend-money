0ef81d28b98d4e00c46f7b58381d8b50af81d244
mathlete01 <msallin@gmail.com>
2021-02-23 00:27:53 -0800

    updated host

--- config.ru
+++ config.ru
@@ -2,10 +2,4 @@
 
 require_relative 'config/environment'
 
-use Rack::Cors do
-    allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
-    end
-end
 run Rails.application
--- config/application.rb
+++ config/application.rb
@@ -32,12 +32,6 @@ module ApiMoney
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-         origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
-       end
-    end
     config.api_only = true
   end
 end
