1cc4f885efb576f677d1a614e41a0209588a80ad
mathlete01 <msallin@gmail.com>
2021-02-23 00:33:08 -0800

    added cors in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
     before_action :authorized
+    before_filter :cors_preflight_check
+    after_filter :cors_set_access_control_headers
   
     def encode_token(payload)
       # should store secret in env variable
@@ -36,5 +38,24 @@ class ApplicationController < ActionController::API
   
     def authorized
       render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+    end
+
+    # For all responses in this controller, return the CORS access control headers.
+
+    def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Max-Age'] = "1728000"
+    end
+
+    def cors_preflight_check
+      if request.method == "OPTIONS"
+        headers['Access-Control-Allow-Origin'] = 'http://localhost'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+        headers['Access-Control-Max-Age'] = '1728000'
+        render :text => '', :content_type => 'text/plain'
+      end
     end
   end
